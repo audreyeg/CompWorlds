4d160b6bc2fb50f1ab0a390aed21a0ecf7583c44
audreyeg <63382246+audreyeg@users.noreply.github.com>
2021-01-26 23:41:18 -0800

    Update OverWorld.js

--- OverWorld.js
+++ OverWorld.js
@@ -49,3 +49,100 @@ class Road
     }
 
 }
+
+class House {
+     constructor(game, x, y, w, h) {
+        Object.assign(this, { game, x, y, w, h });
+        this.game.house = this;
+        // spritesheet
+        this.spritesheet = ASSET_MANAGER.getAsset("./sprites/house.png");
+        this.BB = new BoundingBox(this.x,this.y,this.w,this.h);
+    }
+    update()
+    {
+    }
+    draw(ctx)
+    {
+        ctx.drawImage(this.spritesheet,100,18,310,470,this.x,this.y,this.w,this.h);
+    }
+}
+
+class Saloon {
+     constructor(game, x, y, visible) {
+        Object.assign(this, { game, x, y, visible});
+        this.game.Saloon = this;
+        this.visible = visible;
+        // spritesheet
+        this.spritesheet = ASSET_MANAGER.getAsset("./sprites/saloon.png");
+        this.BB = new BoundingBox(this.x,this.y,350,300);
+    }
+    update()
+    {
+    }
+    draw(ctx)
+    {
+        if (this.visible)
+        {
+             ctx.drawImage(this.spritesheet,0,0,952,1024,this.x,this.y,400,350);
+        }
+       // ctx.drawImage(this.spritesheet,0,0,952,1024,this.x,this.y,400,350);
+    }
+}
+
+class Sheriff {
+     constructor(game, x, y, visible) {
+        Object.assign(this, { game, x, y});
+        this.game.Sheriff = this;
+        this.visible = visible;
+        // spritesheet
+        this.spritesheet = ASSET_MANAGER.getAsset("./sprites/sheriff.png");
+        this.BB = new BoundingBox(this.x,this.y,400,250);
+    }
+    update()
+    {
+    }
+    draw(ctx)
+    {
+        if (this.visible) 
+        {
+            ctx.drawImage(this.spritesheet,0,0,894,512,this.x,this.y,400,250);
+        }
+    }
+}
+
+class Bank {
+     constructor(game, x, y, visible) {
+        Object.assign(this, { game, x, y});
+        this.game.Bank = this;
+        this.visible = visible;
+        // spritesheet
+        this.spritesheet = ASSET_MANAGER.getAsset("./sprites/bank.png");
+        this.BB = new BoundingBox(this.x,this.y,300,300);
+    }
+    update()
+    {
+    }
+    draw(ctx)
+    {
+        if (this.visible)
+        {
+            ctx.drawImage(this.spritesheet,0,0,1438,1086,this.x,this.y,200,300);
+        }
+    }
+}
+
+class Floor {
+       constructor(game, x, y, w, h) {
+       Object.assign(this, {game, x, y, w, h});
+        this.game.Floor = this;
+        this.spritesheet = ASSET_MANAGER.getAsset("./sprites/floor.png");
+        this.BB = new BoundingBox(x,y,w,h);
+    }
+    update() {
+    }
+    draw(ctx) {
+            //spritesheet, xStart, yStart, width, height, x, y, dimensions of box to fill 
+            ctx.drawImage(this.spritesheet, 120, 40, 952, 1200, this.x, this.y, this.w, this.h);
+
+    }
+}
