5f18cf3c582e4842b6c10ee0e10dd39edac7d156
timmy6336 <56135297+timmy6336@users.noreply.github.com>
2021-01-28 12:52:37 -0800

    Update heal.js

--- heal.js
+++ heal.js
@@ -1,22 +1,26 @@
 class Heal{
 
-    constructor(game, x, y) {
+    constructor(game, x, y,animated = false) {
         Object.assign(this, { game, x, y });
         // spritesheet
         this.spritesheet = ASSET_MANAGER.getAsset("./sprites/HealthPickup.png");
         this.x = x;
         this.y = y;
+        this.animated = animated;
         this.lifetime = 45;
         this.BB = new BoundingBox(x,y,30,30);
     };
     update()
     {
-        // if(this.lifetime == 0)
-        // {
-        //     this.removeFromWorld = true;
-        // }
-        // this.lifetime--;
-        // this.y -= 2;
+        if(this.animated)
+        {
+             if(this.lifetime == 0)
+             {
+             this.removeFromWorld = true;
+             }
+             this.lifetime--;
+             this.y -= 2;
+        }
     }
     draw(ctx)
     {
